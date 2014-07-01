7ed4bc958a469eae67ca67585d00d1f3596cb7de
Roman Petrov <rms_biz@RmS-Air.local>
2014-07-01 19:24:11 +0400

    первая версия
    это должно попасть в мастер

---
+++ index.php
@@ -0,0 +1,58 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Правильное-резюме.онлайн");
+?>
+<div class="container">
+<header>
+<a class="login" href="/bitrix/admin/">Войти</a>
+<div class="header-title">Правильное-резюме.онлайн</div>
+<div class="header-subtitle">Твое резюме должно работать на тебя!</div>
+</header>
+<div class="content clearfix">
+<div class="happy">
+<div class="cup"></div>
+<div class="eyes"></div>
+</div>
+<div class="form">
+<form action="" method="post">
+<fieldset>
+<div class="form-title">А у вас правильное резюме?</div>
+<input type="text" name="text" id="" placeholder="Ваш E-mail">
+<button type="submit">Проверить бесплатно</button>
+</fieldset>
+</form>
+</div>
+<div class="sad">
+<div class="eyes"></div>
+<div class="drops"></div>
+<div class="questionmrk img1"></div>
+<div class="questionmrk img2"></div>
+<div class="questionmrk img3"></div>
+<div class="questionmrk img4"></div>
+<div class="questionmrk img5"></div>
+<div class="questionmrk img6"></div>
+</div>
+</div>
+<footer>
+<div class="footer-title">Мы рекомендуем <span>правильное резюме</span> для сайтов:</div>
+<div class="links">
+<div class="link-wrap1">
+<div class="link1" href="http://hh.ru" target="_blank"></div>
+</div>
+<div class="link-wrap2">
+<div class="link2" href="http://superjob.ru" target="_blank"></div>
+</div>
+<div class="link-wrap3">
+<div class="link3" href="http://job.ru" target="_blank"></div>
+</div>
+<div class="link-wrap4">
+<div class="link4" href="http://rabota.ru" target="_blank"></div>
+</div>
+<div class="link-wrap5">
+<div class="link5" href="http://career.ru" target="_blank"></div>
+</div>
+</div>
+</footer>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ index_html.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Resume</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=latin,latin-ext,cyrillic-ext,cyrillic">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster&subset=latin,latin-ext,cyrillic,cyrillic-ext">
+</head>
+<body>
+	<div class="container">
+		<header>
+			<a class="login" href="/bitrix/admin/">Войти</a>		
+			<div class="header-title">Правильное-резюме.онлайн</div>
+			<div class="header-subtitle">Твое резюме должно работать на тебя!</div>			
+		</header>
+		<div class="content clearfix">
+			<div class="happy">
+				<div class="cup"></div>
+				<div class="eyes"></div>
+			</div>
+			<div class="form">
+				<form action="" method="post">
+					<fieldset>
+						<div class="form-title">А у вас правильное резюме?</div>
+						<input type="text" name="text" id="" placeholder="Ваш E-mail">
+						<button type="submit">Проверить бесплатно</button>
+					</fieldset>
+				</form>
+			</div>
+			<div class="sad">
+				<div class="eyes"></div>
+				<div class="drops"></div>
+				<div class="questionmrk img1"></div>
+				<div class="questionmrk img2"></div>
+				<div class="questionmrk img3"></div>
+				<div class="questionmrk img4"></div>
+				<div class="questionmrk img5"></div>
+				<div class="questionmrk img6"></div>
+			</div>
+		</div>
+		<footer>
+			<div class="footer-title">Мы рекомендуем <span>правильное резюме</span> для сайтов:</div>
+			<div class="links">
+				<div class="link-wrap1">
+					<a class="link1" href="http://hh.ru" target="_blank"></a>
+				</div>
+				<div class="link-wrap2">
+					<div class="link2" href="http://superjob.ru" target="_blank"></div>
+				</div>
+				<div class="link-wrap3">
+					<a class="link3" href="http://job.ru" target="_blank"></a>
+				</div>
+				<div class="link-wrap4">
+					<div class="link4" href="http://rabota.ru" target="_blank"></div>
+				</div>
+				<div class="link-wrap5">
+					<a class="link5" href="http://career.ru" target="_blank"></a>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
