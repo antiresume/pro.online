aa66ac094074a1bf806d7c6b677c2652b9ee1cb8
Roman Petrov <rms_biz@RmS-Air.local>
2014-07-06 14:59:13 +0400

    404
    добавил страницу 404

--- 404.php
+++ 404.php
@@ -8,13 +8,15 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
 
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"36000000"
-	)
-);
+?>
+<div class="container">
+<header>
+<a class="login" href="/bitrix/admin/">Войти</a>
+<div class="header-title"><a href="/">Правильное-резюме.онлайн</a></div>
+<div class="header-subtitle">Твое резюме должно работать на тебя!</div>
+</header>
+<div class="once">К сожалению, страница не найдена</div>
+</div>
+<?
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,17 +1,6 @@
 <?
 $arUrlRewrite = array(
-	array(
-		"CONDITION" => "#^/photo/#",
-		"RULE" => "",
-		"ID" => "bitrix:photogallery",
-		"PATH" => "/photo.php",
-	),
-	array(
-		"CONDITION" => "#^/#",
-		"RULE" => "",
-		"ID" => "bitrix:blog",
-		"PATH" => "/index.php",
-	),
+
 );
 
 ?>
