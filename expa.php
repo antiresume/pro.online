666e938f347b92643a7ce82bff4b8144f32da533
Roman Petrov <rms_biz@RmS-Air.local>
2014-08-15 15:46:24 +0400

    new resume

---
+++ expa.php
@@ -0,0 +1,14 @@
+<?
+include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$APPLICATION->SetTitle("Оплата экспертизы резюме");
+
+
+$sum = 90;
+
+include($_SERVER["DOCUMENT_ROOT"]."/exp_inc.php");
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
